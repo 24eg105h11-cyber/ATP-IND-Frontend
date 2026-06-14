724e43fe37e9685b11c0ef186c0a03e46c967bf6
24eg107d08-venu <24eg105h11@anurag.edu.in>
2026-06-14 22:49:27 +0530

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react'
-import { BrowserRouter as Router, Routes, Route, Navigate, Link } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route, Navigate, Link } from 'react-router-dom'
 import CodePlayground from './components/CodePlayground'
 import LoginPage from './components/LoginPage'
 import RegistrationPage from './components/RegistrationPage'
